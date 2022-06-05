9501212bb45d09b248b879402edef45d25692e79
cruz <102335673+cruzann@users.noreply.github.com>
2022-06-05 03:39:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5579882344:AAEM9jox3Thrt-5OLKzd8xEcjnkG9vD1wT4"
-APP_URL = "https://ikop09vvt12.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5331598098:AAHuQhbn9NDJYN-ZbJAVD7GUJTqSQ6Qiv3U"
+APP_URL = "https://bnhuji99k0.herokuapp.com/" + BOT_TOKEN
